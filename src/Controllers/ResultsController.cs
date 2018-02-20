6938c56ebc35124ec2cdfdadabdddd4f8e56e555
Ashley Baldock <ashley@baldock.me>
2018-02-20 14:46:38 +0000

    Fix subjects consumption from Api

--- src/Controllers/ResultsController.cs
+++ src/Controllers/ResultsController.cs
@@ -3,6 +3,9 @@ using GovUk.Education.SearchAndCompare.Domain.Filters;
 using GovUk.Education.SearchAndCompare.UI.ViewModels;
 using GovUk.Education.SearchAndCompare.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using GovUk.Education.SearchAndCompare.Domain.Lists;
+using GovUk.Education.SearchAndCompare.Domain.Models;
+using System.Linq;
 
 namespace GovUk.Education.SearchAndCompare.UI.Controllers
 {
@@ -21,11 +24,21 @@ namespace GovUk.Education.SearchAndCompare.UI.Controllers
         {
             var courses = _api.GetCourses(filter);
 
-            var subjects = _api.GetSubjects(filter);
+            var subjects = _api.GetSubjects();
+
+            FilteredList<Subject> filteredSubjects;
+            if (filter.SelectedSubjects.Count > 0) {
+                filteredSubjects = new FilteredList<Subject>(
+                    subjects.Where(subject => filter.SelectedSubjects.Contains(subject.Id)).ToList(),
+                    subjects.Count
+                );
+            } else {
+                filteredSubjects = new FilteredList<Subject>(subjects, subjects.Count);
+            }
 
             var viewModel = new ResultsViewModel {
                 Courses = courses,
-                Subjects = subjects,
+                Subjects = filteredSubjects,
                 FilterModel = filter
             };
 
