f4e939497331148378765298893530b9f1ae9ed1
Ashley Baldock <ashley@baldock.me>
2018-02-20 10:42:36 +0000

    Fix build on travis

--- travis-build.sh
+++ travis-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 dotnet restore ./src/SearchAndCompareUi.csproj
-dotnet restore ./tests/Unit.Tests.csproj
-dotnet test ./tests/Unit.Tests.csproj
+dotnet restore ./tests/SearchAndCompareUI.Tests.csproj
+dotnet test ./tests/SearchAndCompareUI.Tests.csproj
 
 #Explicily run [Explicit] tests
-dotnet test ./tests/Unit.Tests.csproj --filter TestCategory="Integration"
-dotnet test ./tests/Unit.Tests.csproj --filter TestCategory="APIs"
+dotnet test ./tests/SearchAndCompareUI.Tests.csproj --filter TestCategory="Integration"
+dotnet test ./tests/SearchAndCompareUI.Tests.csproj --filter TestCategory="APIs"
