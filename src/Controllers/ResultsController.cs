097337f81661e754b53de386bc829d78a15cdf5d
Ashley Baldock <ashley@baldock.me>
2018-03-06 17:43:44 +0000

    Add viewmodels and frontend for maps

--- src/Controllers/ResultsController.cs
+++ src/Controllers/ResultsController.cs
@@ -8,25 +8,65 @@ using GovUk.Education.SearchAndCompare.Domain.Models;
 using System.Linq;
 using GovUk.Education.SearchAndCompare.UI.ActionFilters;
 using GovUk.Education.SearchAndCompare.UI.Filters;
+using System.Threading.Tasks;
+using GovUk.Education.SearchAndCompare.ViewFormatters;
+using GovUk.Education.SearchAndCompare.UI.Services.Maps.Models;
+using System.Collections.Generic;
+using GovUk.Education.SearchAndCompare.UI.Services.Maps;
+using GovUk.Education.SearchAndCompare.ViewModels;
 
 namespace GovUk.Education.SearchAndCompare.UI.Controllers
 {
     //[Authorize]
     public class ResultsController : CommonAttributesControllerBase
     {
-        private readonly ISearchAndCompareApi _api;
+        private readonly ISearchAndCompareApi api;
+        private readonly IMapProvider mapProvider;
 
-        public ResultsController(ISearchAndCompareApi api)
+        public ResultsController(ISearchAndCompareApi api, IMapProvider mapProvider)
         {
-            _api = api;
+            this.api = api;
+            this.mapProvider = mapProvider;
+        }
+
+        [HttpPost("results/map")]
+        public IActionResult Map(QueryFilter filter)
+        {
+            return RedirectToAction("Index", "Results", filter.ToRouteValues());
+        }
+
+        private static List<CourseGroup> GroupByProvider(PaginatedList<Course> courses)
+        {
+            return courses.Items.GroupBy(
+                course => course.ProviderLocation.AsCoordinates(),
+                course => course,
+                (key, groupedCourses) => new CourseGroup {
+                    Courses = groupedCourses.ToList(),
+                    Coordinates = key
+                }
+            ).ToList();
+        }
+
+        [HttpGet("results/mapimage")]
+        public async Task<IActionResult> MapImage(QueryFilter filter)
+        {
+            var courses = api.GetCourses(filter);
+            var courseGroups = GroupByProvider(courses);
+
+            var mapProjection = mapProvider.GetMapProjection<CourseGroup>(
+                courseGroups, filter.Coordinates, 400, filter.zoomlevel);
+
+            byte[] content = await mapProvider.GetStaticMapImageAsync(mapProjection);
+            //return "data:image/png;base64," + Convert.ToBase64String(content);
+            return File(content, "image/png");
         }
 
         [HttpGet("results")]
         public IActionResult Index(QueryFilter filter)
         {
-            var courses = _api.GetCourses(filter);
+            var courses = api.GetCourses(filter);
 
-            var subjects = _api.GetSubjects();
+            var subjects = api.GetSubjects();
 
             FilteredList<Subject> filteredSubjects;
             if (filter.SelectedSubjects.Count > 0) {
@@ -39,10 +79,25 @@ namespace GovUk.Education.SearchAndCompare.UI.Controllers
             }
 
             var viewModel = new ResultsViewModel {
-                Courses = courses,
                 Subjects = filteredSubjects,
-                FilterModel = filter
+                FilterModel = filter,
+                Courses = courses
             };
+
+            if (filter.DisplayAsMap)
+            {
+                var courseGroups = GroupByProvider(courses);
+
+                var mapProjection = mapProvider.GetMapProjection<CourseGroup>(
+                    courseGroups, filter.Coordinates, 400, filter.zoomlevel);
+
+                viewModel.Map = new MapViewModel {
+                    CourseGroups = mapProjection.MarkersWithAreas,
+                    MyLocation = filter.Coordinates,
+                    Map = mapProjection,
+                    MapsApiKey = mapProvider.ApiKey
+                };
+            }
 
             return View(viewModel);
         }
--- src/ViewModels/ResultsViewModel.cs
+++ src/ViewModels/ResultsViewModel.cs
@@ -1,11 +1,14 @@
 using GovUk.Education.SearchAndCompare.Domain.Lists;
 using GovUk.Education.SearchAndCompare.Domain.Models;
 using GovUk.Education.SearchAndCompare.Domain.Filters;
+using GovUk.Education.SearchAndCompare.ViewModels;
 
 namespace GovUk.Education.SearchAndCompare.UI.ViewModels
 {
     public class ResultsViewModel
     {
+        public MapViewModel Map { get; set; }
+
         public PaginatedList<Course> Courses { get; set; }
 
         public FilteredList<Subject> Subjects { get; set; }
