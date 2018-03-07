e1706d036b7faabd1684db8064de779d456c3cca
the-daniel-rothig <daniel.rothig@gmail.com>
2018-03-07 11:45:44 +0000

    add qualifications to view and controller

--- src/Controllers/FilterController.cs
+++ src/Controllers/FilterController.cs
@@ -172,5 +172,19 @@ namespace GovUk.Education.SearchAndCompare.UI.Controllers
             ViewBag.IsInWizard = true;
             return Funding(applyFilter, model);
         }
+
+        [HttpGet("results/filter/qualification")]
+        [ActionName("Qualification")]
+        public IActionResult QualificationGet(QueryFilter model)
+        {
+            return View(model);
+        }
+        
+        [HttpPost("results/filter/qualification")]
+        [ActionName("Qualification")]
+        public IActionResult QualificationPost(QueryFilter model)
+        {
+            return RedirectToAction("Index", "Results", model.ToRouteValues());
+        }
     }
 }
