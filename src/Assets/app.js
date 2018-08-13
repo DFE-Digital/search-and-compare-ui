6b131fb95069e33a1ea186acb634636b28c00c60
Dan Mitchell <dan@danmitchell.co.uk>
2018-08-13 14:03:26 +0100

    Move js into file and constructor

--- src/Assets/app.js
+++ src/Assets/app.js
@@ -1,4 +1,5 @@
 import { initAll } from 'govuk-frontend';
+import BackLink from './Javascript/back-link';
 import './Javascript/cookie-bar.js';
 import './Javascript/accordion.js';
 import './Javascript/toggle.js';
@@ -6,6 +7,9 @@ import './Javascript/typeahead.jquery.js';
 import './Styles/site.scss';
 
 initAll();
+
+var $backLink = document.querySelector('[data-module="back-link"]')
+new BackLink($backLink).init()
 
 if (!!$) {
   $(document).ready(function () {
