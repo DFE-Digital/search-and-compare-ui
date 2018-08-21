b0c82e0a94ea3254d119ce2039a7a6b9d0aa543c
Tim Abell <tim@timwise.co.uk>
2018-08-21 15:43:26 +0100

    Configure SeriLog to send to ApplicationInsightsEvents
    Bring Program.cs into line with latest from manage courses repos

--- README.md
+++ README.md
@@ -15,3 +15,27 @@ Defaults to published development API backend to make it easier to get started, 
 Note: The `API_URI` value musn't have a trailing slash.
 
     cd src && set API_URI=http://localhost:5001/api && dotnet run
+
+## Logging
+
+Logging is configured in `appsettings.json`, and values in there can be overridden with environment variables.
+
+Powershell:
+
+    $env:Serilog:MinimumLevel="Debug"
+    dotnet run
+
+Command prompt
+
+    set Serilog:MinimumLevel=Debug
+    dotnet run
+
+For more information see:
+
+* https://github.com/serilog/serilog-settings-configuration
+* https://nblumhardt.com/2016/07/serilog-2-minimumlevel-override/
+
+Serilog has been configured to spit logs out to both the console
+(for `dotnet run` testing & development locally) and Application Insights.
+
+Set the `APPINSIGHTS_INSTRUMENTATIONKEY` environment variable to tell Serilog the application insights key.
--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace GovUk.Education.SearchAndCompare.UI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo
+                .ApplicationInsightsTraces(configuration["APPINSIGHTS_INSTRUMENTATIONKEY"])
+                .CreateLogger();
+
+            var programLogger = Log.ForContext<Program>();
+
+            try
+            {
+                programLogger.Information("Starting web host");
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                programLogger.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseApplicationInsights()
-                .UseStartup<Startup>();
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                })
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
+
+        private static IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
     }
 }
