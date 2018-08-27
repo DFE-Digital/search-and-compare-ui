1a4bbc49ef341018a539455c09a06ab1a1166db8
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-27 13:48:44 +0100

    missing section

--- shared/ViewComponents/CourseDetailsViewComponent.cs
+++ shared/ViewComponents/CourseDetailsViewComponent.cs
@@ -40,11 +40,12 @@ namespace GovUk.Education.SearchAndCompare.UI.Shared.ViewComponents
     public static class CourseDetailsSections
     {
         public const string AboutTheCourse = "about this training programme";
+        public const string InterviewProcess = "interview process";
         public const string EntryRequirementsQualifications = "entry requirements";
         public const string EntryRequirementsPersonalQualities = "entry requirements personal qualities";
         public const string EntryRequirementsOther = "entry requirements other";
         public const string ListOfSchools = "placement schools";
-        public const string AboutSchools = "about placement schools";
+        public const string AboutSchools = "about school placements";
         public const string AboutFees = "about fees";
         public const string AboutTheProvider = "about this training provider";
         public const string AboutTheAccreditingProvider = "about this training provider accrediting";        
