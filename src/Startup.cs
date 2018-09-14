952e367760ea6f558ce6eae6175be77d35d88b57
Tim Abell <tim@timwise.co.uk>
2018-09-14 14:21:02 +0100

    Removed accidental whitespace

--- src/Startup.cs
+++ src/Startup.cs
@@ -34,7 +34,6 @@ namespace GovUk.Education.SearchAndCompare.UI
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
