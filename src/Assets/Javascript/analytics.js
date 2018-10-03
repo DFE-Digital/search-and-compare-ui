250a9e247c1ef1f07ed8c905dadbd2663bf5e700
Theodor Vararu <theo@vararu.org>
2018-10-03 12:33:31 +0100

    Wrap form analytics in an initialiser function
    This is an intermediary step before adding external link analytics,
    and will help with refactoring this further into its own separate file
    when necessary.

--- src/Assets/Javascript/analytics.js
+++ src/Assets/Javascript/analytics.js
@@ -1,39 +1,43 @@
-// How to use:
-// 1. Attach `data-ga-event-form="Helpful Namespace"` to <form> elements
-// 2. Attach `data-ga-event-form-input="Helpful Label" to <input>s of type checkbox or radio
-// When users click on [type='submit'] in the form, an analytics event is triggered with the
-// namespace followed by the specified labels of the checked inputs.
-const dataAttrForm = "data-ga-event-form"
-const dataAttrInput = "data-ga-event-form-input"
+const initFormAnalytics = () => {
+  // How to use:
+  // 1. Attach `data-ga-event-form="Helpful Namespace"` to <form> elements
+  // 2. Attach `data-ga-event-form-input="Helpful Label" to <input>s of type checkbox or radio
+  // When users click on [type='submit'] in the form, an analytics event is triggered with the
+  // namespace followed by the specified labels of the checked inputs.
+  const dataAttrForm = "data-ga-event-form"
+  const dataAttrInput = "data-ga-event-form-input"
 
-const triggerAnalyticsEvent = (namespace, label) => {
-  ga("send", "event", {
-    eventCategory: "Form: " + namespace,
-    eventAction: label,
-    transport: "beacon"
-  })
-}
+  const triggerAnalyticsEvent = (category, action) => {
+    ga("send", "event", {
+      eventCategory: category,
+      eventAction: action,
+      transport: "beacon"
+    })
+  }
 
-const attachAnalyticsToForm = $form => {
-  const namespace = $form.getAttribute(dataAttrForm)
-  const $submitBtn = $form.querySelector('[type="submit"]')
+  const attachAnalyticsToForm = $form => {
+    const namespace = $form.getAttribute(dataAttrForm)
+    const $submitBtn = $form.querySelector('[type="submit"]')
 
-  $submitBtn.addEventListener("click", () => {
-    const $checkedInputs = $form.querySelectorAll("[" + dataAttrInput + "]:checked")
-    const values = []
+    $submitBtn.addEventListener("click", () => {
+      const $checkedInputs = $form.querySelectorAll("[" + dataAttrInput + "]:checked")
+      const values = []
 
-    for (let j = 0; j < $checkedInputs.length; j++) {
-      values.push($checkedInputs[j].getAttribute(dataAttrInput))
-    }
+      for (let j = 0; j < $checkedInputs.length; j++) {
+        values.push($checkedInputs[j].getAttribute(dataAttrInput))
+      }
 
-    triggerAnalyticsEvent(namespace, values.join(", "))
-  })
-}
+      triggerAnalyticsEvent("Form: " + namespace, values.join(", "))
+    })
+  }
 
-if (typeof ga !== "undefined") {
   const $forms = document.querySelectorAll("[" + dataAttrForm + "]")
 
   for (let i = 0; i < $forms.length; i++) {
     attachAnalyticsToForm($forms[i])
   }
+}
+
+if (typeof ga !== "undefined") {
+  initFormAnalytics()
 }
