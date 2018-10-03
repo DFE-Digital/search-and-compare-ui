35dbae18762533ae93557cf8ffbd0427371c96d6
Theodor Vararu <theo@vararu.org>
2018-10-03 12:34:37 +0100

    Add external link analytics
    This is a stripped down version of the external link tracker from
    govuk_frontend_toolkit[0]. A notable difference is this module doesn't
    currently work with links that are dynamically inserted into the DOM
    after page load. Because our application is fully server-side rendered
    at the moment, this can be remedied later if necessary.
    
    - [0]: https://github.com/alphagov/govuk_frontend_toolkit/blob/master/javascripts/govuk/analytics/external-link-tracker.js

--- src/Assets/Javascript/analytics.js
+++ src/Assets/Javascript/analytics.js
@@ -1,3 +1,11 @@
+const triggerAnalyticsEvent = (category, action) => {
+  ga("send", "event", {
+    eventCategory: category,
+    eventAction: action,
+    transport: "beacon"
+  })
+}
+
 const initFormAnalytics = () => {
   // How to use:
   // 1. Attach `data-ga-event-form="Helpful Namespace"` to <form> elements
@@ -6,14 +14,6 @@ const initFormAnalytics = () => {
   // namespace followed by the specified labels of the checked inputs.
   const dataAttrForm = "data-ga-event-form"
   const dataAttrInput = "data-ga-event-form-input"
-
-  const triggerAnalyticsEvent = (category, action) => {
-    ga("send", "event", {
-      eventCategory: category,
-      eventAction: action,
-      transport: "beacon"
-    })
-  }
 
   const attachAnalyticsToForm = $form => {
     const namespace = $form.getAttribute(dataAttrForm)
@@ -38,6 +38,21 @@ const initFormAnalytics = () => {
   }
 }
 
+const initExternalLinkAnalytics = () => {
+  const externalLinkSelector = 'a[href^="http"]:not(a[href*="' + window.location.hostname + '"])'
+
+  const trackClickEvent = event => {
+    triggerAnalyticsEvent("External Link Clicked", event.target.getAttribute("href"))
+  }
+
+  const $links = document.querySelectorAll(externalLinkSelector)
+
+  for (let i = 0; i < $links.length; i++) {
+    $links[i].addEventListener("click", trackClickEvent)
+  }
+}
+
 if (typeof ga !== "undefined") {
   initFormAnalytics()
+  initExternalLinkAnalytics()
 }
