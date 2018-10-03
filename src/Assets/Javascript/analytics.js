9433c0b284c7e657ffcbd01f1ae9baf483bc397b
Theodor Vararu <theo@vararu.org>
2018-10-03 15:02:11 +0100

    Attach analytics to navigation events
    This is to track which parts of sections users navigate to.

--- src/Assets/Javascript/analytics.js
+++ src/Assets/Javascript/analytics.js
@@ -8,10 +8,13 @@ const triggerAnalyticsEvent = (category, action) => {
 
 const initFormAnalytics = () => {
   // How to use:
+  //
   // 1. Attach `data-ga-event-form="Helpful Namespace"` to <form> elements
   // 2. Attach `data-ga-event-form-input="Helpful Label" to <input>s of type checkbox or radio
+  //
   // When users click on [type='submit'] in the form, an analytics event is triggered with the
   // namespace followed by the specified labels of the checked inputs.
+
   const dataAttrForm = "data-ga-event-form"
   const dataAttrInput = "data-ga-event-form-input"
 
@@ -39,6 +42,8 @@ const initFormAnalytics = () => {
 }
 
 const initExternalLinkAnalytics = () => {
+  // This will attach event listeners to all links with hrefs that point to external resource.
+
   const externalLinkSelector = 'a[href^="http"]:not(a[href*="' + window.location.hostname + '"])'
 
   const trackClickEvent = event => {
@@ -52,7 +57,39 @@ const initExternalLinkAnalytics = () => {
   }
 }
 
+const initNavigationAnalytics = () => {
+  // How to use:
+  //
+  // 1. Attach `data-ga-event-navigation="Helpful Namespace"` to a container that has links
+  //
+  // When users click on a[href] in the container, an analytics event is triggered with the
+  // namespace followed by the href.
+
+  const dataAttrNav = "data-ga-event-navigation"
+
+  const attachAnalyticsToNav = $nav => {
+    const namespace = $nav.getAttribute(dataAttrNav)
+
+    const trackClickEvent = event => {
+      triggerAnalyticsEvent(namespace + " Navigation Link Clicked", event.target.getAttribute("href"))
+    }
+
+    const $links = $nav.querySelectorAll("a[href]")
+
+    for (let i = 0; i < $links.length; i++) {
+      $links[i].addEventListener("click", trackClickEvent)
+    }
+  }
+
+  const $navs = document.querySelectorAll("[" + dataAttrNav + "]")
+
+  for (let i = 0; i < $navs.length; i++) {
+    attachAnalyticsToNav($navs[i])
+  }
+}
+
 if (typeof ga !== "undefined") {
   initFormAnalytics()
   initExternalLinkAnalytics()
+  initNavigationAnalytics()
 }
