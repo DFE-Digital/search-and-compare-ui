d36c872a5d8d4c81d9ea378fbbddf5bce11478c4
Martin Bentley <martinbentley@btinternet.com>
2018-10-08 11:30:03 +0100

    Added Feature Flags

---
+++ shared/Features/FeatureFlags.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace GovUk.Education.SearchAndCompare.UI.Shared.Features
+{
+    public class FeatureFlags : IFeatureFlags
+    {
+        private readonly IConfigurationSection _config;
+        private const string APPLY_2019 = "APPLY_2019";
+
+        public FeatureFlags(IConfigurationSection config)
+        {
+            _config = config;
+        }
+        public bool Apply2019 => ShouldShow(APPLY_2019);
+
+        private bool ShouldShow(string key) => _config.GetValue(key, false);
+    }
+}
---
+++ shared/Features/IFeatureFlags.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GovUk.Education.SearchAndCompare.UI.Shared.Features
+{
+    public interface IFeatureFlags
+    {
+        bool Apply2019 { get; }
+    }
+}
