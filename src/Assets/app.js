6295986721ed133c41aa8e28b6a63252b5c62099
Theodor Vararu <theo@vararu.org>
2018-10-10 16:42:43 +0100

    prettier src/Assets/app.js

--- src/Assets/app.js
+++ src/Assets/app.js
@@ -1,30 +1,30 @@
-import { initAll } from 'govuk-frontend';
-import CookieMessage from './Javascript/cookie-message';
-import BackLink from './Javascript/back-link';
-import Accordion from './Javascript/accordion';
-import Toggle from './Javascript/toggle';
-import './Javascript/analytics.js';
-import './Javascript/map.js';
-import './Javascript/typeahead.jquery.js';
-import './Styles/site.scss';
+import { initAll } from "govuk-frontend"
+import CookieMessage from "./Javascript/cookie-message"
+import BackLink from "./Javascript/back-link"
+import Accordion from "./Javascript/accordion"
+import Toggle from "./Javascript/toggle"
+import "./Javascript/analytics.js"
+import "./Javascript/map.js"
+import "./Javascript/typeahead.jquery.js"
+import "./Styles/site.scss"
 
-initAll();
+initAll()
 
-var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
-new CookieMessage($cookieMessage).init();
+var $cookieMessage = document.querySelector('[data-module="cookie-message"]')
+new CookieMessage($cookieMessage).init()
 
-var $backLink = document.querySelector('[data-module="back-link"]');
-new BackLink($backLink).init();
+var $backLink = document.querySelector('[data-module="back-link"]')
+new BackLink($backLink).init()
 
 var $accordions = document.querySelectorAll('[data-module="accordion"]')
 for (var i = $accordions.length - 1; i >= 0; i--) {
   var $accordion = $accordions[i]
-  var $sections = $accordion.querySelectorAll('.accordion-section')
+  var $sections = $accordion.querySelectorAll(".accordion-section")
   for (var j = $sections.length - 1; j >= 0; j--) {
-    var $section = $sections[j];
+    var $section = $sections[j]
     var sectionContainsCheckedCheckboxes = !!$section.querySelector('.govuk-checkboxes input[type="checkbox"]:checked')
     if (sectionContainsCheckedCheckboxes) {
-      $section.classList.add('accordion-section--expanded')
+      $section.classList.add("accordion-section--expanded")
     }
   }
   try {
@@ -32,43 +32,50 @@ for (var i = $accordions.length - 1; i >= 0; i--) {
   } catch (e) {
     for (var j = $sections.length - 1; j >= 0; j--) {
       var $section = $sections[j]
-      $section.classList.remove('accordion-section--hidden')
-      $section.classList.add('accordion-section--expanded')
+      $section.classList.remove("accordion-section--hidden")
+      $section.classList.add("accordion-section--expanded")
     }
   }
-};
+}
 
 var $toggle = document.querySelectorAll('[data-module="toggle"]')
 for (var i = $toggle.length - 1; i >= 0; i--) {
-  new Toggle($toggle[i]).init();
-};
+  new Toggle($toggle[i]).init()
+}
 
 if (!!$) {
-  $(document).ready(function () {
+  $(document).ready(function() {
     // Turn off jQuery animation
     jQuery.fx.off = true
 
-    $('.typeahead').each(function () {
-      var $this = $(this);
-      var url = $this.data("url");
-      $this.typeahead({
-        minLength: 3,
-        highlight: true
-      }, {
-        name: url.replace(/\//g, "-"),
-        source: function (query, cbSync, cbAsync) {
-          $.get(url, {
-            query: query
-          }, function (res) {
-            cbAsync(res);
-          });
+    $(".typeahead").each(function() {
+      var $this = $(this)
+      var url = $this.data("url")
+      $this.typeahead(
+        {
+          minLength: 3,
+          highlight: true
         },
-        limit: 10
-      });
-    });
+        {
+          name: url.replace(/\//g, "-"),
+          source: function(query, cbSync, cbAsync) {
+            $.get(
+              url,
+              {
+                query: query
+              },
+              function(res) {
+                cbAsync(res)
+              }
+            )
+          },
+          limit: 10
+        }
+      )
+    })
   })
 }
 
-if (process.env.NODE_ENV == 'development') {
-  module.hot.accept();
+if (process.env.NODE_ENV == "development") {
+  module.hot.accept()
 }
