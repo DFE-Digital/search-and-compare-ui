16b276c7b123840935c10707ed351a0adf6ae581
Theodor Vararu <theo@vararu.org>
2018-10-25 15:39:48 +0100

    Customise the map markers
    - Use labels instead of pin icons
    - Expand the label into a full window when you click on the pin

--- src/Assets/Javascript/map.js
+++ src/Assets/Javascript/map.js
@@ -1,4 +1,113 @@
+const createPopupClass = () => {
+  const panToWithOffset = function(map, latlng, offsetX, offsetY) {
+    var ov = new google.maps.OverlayView()
+    ov.onAdd = function() {
+      var proj = this.getProjection()
+      var aPoint = proj.fromLatLngToContainerPixel(latlng)
+      aPoint.x = aPoint.x + offsetX
+      aPoint.y = aPoint.y + offsetY
+      map.panTo(proj.fromContainerPixelToLatLng(aPoint))
+    }
+    ov.draw = function() {}
+    ov.setMap(map)
+  }
+
+  /**
+   * A customized popup on the map.
+   * @param {!google.maps.LatLng} position
+   * @param {!Element} content
+   * @constructor
+   * @extends {google.maps.OverlayView}
+   */
+  const Popup = function(position, closedContent, openContent) {
+    this.position = position
+    const content = document.createElement("div")
+    content.classList.add("popup-bubble-content")
+    closedContent.classList.add("popup-bubble__closed-content")
+    openContent.classList.add("popup-bubble__open-content")
+    content.appendChild(closedContent)
+    content.appendChild(openContent)
+
+    var pixelOffset = document.createElement("div")
+    pixelOffset.classList.add("popup-bubble-anchor")
+    pixelOffset.appendChild(content)
+
+    this.anchor = document.createElement("div")
+    this.anchor.classList.add("popup-tip-anchor")
+    this.anchor.appendChild(pixelOffset)
+
+    // Optionally stop clicks, etc., from bubbling up to the map.
+    this.stopEventPropagation()
+
+    closedContent.addEventListener("click", e => {
+      panToWithOffset(this.getMap(), this.position, 0, -150)
+      this.closeOpenPopups()
+      this.anchor.classList.toggle("open")
+      e.stopPropagation()
+    })
+
+    const $closeButton = openContent.querySelector(".popup-bubble__close")
+    $closeButton.addEventListener("click", e => {
+      console.log("hello", e)
+      this.closeOpenPopups()
+    })
+  }
+  // NOTE: google.maps.OverlayView is only defined once the Maps API has
+  // loaded. That is why Popup is defined inside initMap().
+  Popup.prototype = Object.create(google.maps.OverlayView.prototype)
+
+  /** Called when the popup is added to the map. */
+  Popup.prototype.onAdd = function() {
+    this.getPanes().floatPane.appendChild(this.anchor)
+  }
+
+  /** Called when the popup is removed from the map. */
+  Popup.prototype.onRemove = function() {
+    if (this.anchor.parentElement) {
+      this.anchor.parentElement.removeChild(this.anchor)
+    }
+  }
+
+  /** Called when the popup needs to draw itself. */
+  Popup.prototype.draw = function() {
+    var divPosition = this.getProjection().fromLatLngToDivPixel(this.position)
+    // Hide the popup when it is far out of view.
+    var display = Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ? "block" : "none"
+
+    if (display === "block") {
+      this.anchor.style.left = divPosition.x + "px"
+      this.anchor.style.top = divPosition.y + "px"
+    }
+    if (this.anchor.style.display !== display) {
+      this.anchor.style.display = display
+    }
+  }
+
+  Popup.prototype.closeOpenPopups = () => {
+    const $anchors = document.querySelectorAll(".popup-tip-anchor.open")
+    for (let i = 0; i < $anchors.length; i++) {
+      $anchors[i].classList.remove("open")
+    }
+  }
+
+  /** Stops clicks/drags from bubbling up to the map. */
+  Popup.prototype.stopEventPropagation = function() {
+    var anchor = this.anchor
+    anchor.style.cursor = "auto"
+    ;[("click", "dblclick", "contextmenu", "wheel", "mousedown", "touchstart", "pointerdown")].forEach(function(event) {
+      anchor.addEventListener(event, function(e) {
+        console.log(event, e)
+        e.stopPropagation()
+      })
+    })
+  }
+
+  return Popup
+}
+
 const initGoogleMaps = () => {
+  const Popup = createPopupClass()
+
   const bounds = new google.maps.LatLngBounds()
 
   let $zoomLevel
@@ -39,20 +148,6 @@ const initGoogleMaps = () => {
     }
   })
 
-  const locationMarker = {
-    url: "/images/map-marker-black.png",
-    scaledSize: new google.maps.Size(20, 32),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(0, 32)
-  }
-
-  const searchMarker = {
-    url: "/images/map-marker-red.png",
-    scaledSize: new google.maps.Size(30, 44),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(12, 32)
-  }
-
   const textMarker = {
     url: "/images/map-marker-text.png",
     scaledSize: new google.maps.Size(60, 22),
@@ -61,56 +156,39 @@ const initGoogleMaps = () => {
   }
 
   const locations = window.locations
-  const infoWindow = new google.maps.InfoWindow()
-  const markers = []
-
-  google.maps.event.addListener(map, "click", function() {
-    infoWindow.close()
-  })
 
   for (let i = 0, length = locations.length; i < length; i++) {
     const data = locations[i]
     const latLng = new google.maps.LatLng(data.lat, data.lng)
 
-    const marker = new google.maps.Marker({
-      position: latLng,
-      map,
-      title: data.title,
-      icon: locationMarker,
-      zIndex: 2
-    })
-    ;((marker, data) => {
-      google.maps.event.addListener(marker, "click", e => {
-        const windowsContent = `
-          <div class="search-info-window">
-            ${
-              data.no_of_courses > 1
-                ? `<h3 class="govuk-heading-s">${data.title}</h3>`
-                : `<h3 class="govuk-heading-s">${data.courses[0].name} with ${data.title}</h3>`
-            }
-            ${data.no_of_courses > 1 ? `<h4 class="govuk-heading-s">${data.no_of_courses} courses</h4>` : ""}
-            <ul class="govuk-list">
-              ${data.courses
-                .map(
-                  course =>
-                    `<li>
-                  <a href="${course.url}">${course.name} (${course.code})</a>
-                  <br>
-                  ${course.qual}
-                </li>`
-                )
-                .join("")}
-            </ul>
-          </div>
-        `
+    const closedContent = document.createElement("div")
+    closedContent.innerHTML = data.title
 
-        infoWindow.setContent(windowsContent)
-        infoWindow.setOptions({ maxWidth: 250 })
-        infoWindow.open(map, marker)
-      })
-    })(marker, data)
+    const openContent = document.createElement("div")
+    openContent.innerHTML = `
+      ${
+        data.no_of_courses > 1
+          ? `<h3 class="govuk-heading-s">${data.title}</h3>`
+          : `<h3 class="govuk-heading-s">${data.courses[0].name} with ${data.title}</h3>`
+      }
+      ${data.no_of_courses > 1 ? `<h4 class="govuk-heading-s">${data.no_of_courses} courses</h4>` : ""}
+      <ul class="govuk-list">
+        ${data.courses
+          .map(
+            course =>
+              `<li>
+            <a href="${course.url}">${course.name} (${course.code})</a>
+            <br>
+            ${course.qual}
+          </li>`
+          )
+          .join("")}
+      </ul>
+      <button class="popup-bubble__close">&times;<span class="govuk-visually-hidden">Close this popup</span></button>
+    `
 
-    markers.push(marker)
+    const popup = new Popup(latLng, closedContent, openContent)
+    popup.setMap(map)
 
     // Extend the bounds by the first 10 locations so we get a decent number as part of the first view.
     if (i < 10) {
@@ -121,8 +199,7 @@ const initGoogleMaps = () => {
   // Add marker for search location
   new google.maps.Marker({
     position: map.getCenter(),
-    map: map,
-    icon: searchMarker
+    map: map
   })
 
   // 5 miles, 10 miles, 20 miles
@@ -153,30 +230,6 @@ const initGoogleMaps = () => {
       zIndex: 1
     })
   }
-
-  const $icons = {
-    search_location: {
-      name: "Your location",
-      icon: searchMarker.url
-    },
-    campus: {
-      name: "Training locations",
-      icon: locationMarker.url
-    }
-  }
-
-  const legend = document.getElementById("legend")
-  const legendList = legend.querySelector("ul")
-  for (let key in $icons) {
-    var type = $icons[key]
-    var name = type.name
-    var icon = type.icon
-    var listItem = document.createElement("li")
-    listItem.innerHTML = `<img width="20" src="${icon}"> ${name}`
-    legendList.appendChild(listItem)
-  }
-
-  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend)
 
   if (locations.length > 1) {
     map.fitBounds(bounds)
