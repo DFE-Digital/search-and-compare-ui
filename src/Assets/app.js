050d9aaab774f2e515d2c466bf330becfa3be8b4
Dan Mitchell <dan@danmitchell.co.uk>
2018-11-19 12:15:57 +0000

    Clean up and lint app js (#279)

--- src/Assets/app.js
+++ src/Assets/app.js
@@ -16,19 +16,21 @@ initAll();
 window.initGoogleMaps = initGoogleMaps;
 window.initLocationsMap = initLocationsMap;
 
-var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
+const $cookieMessage = document.querySelector('[data-module="cookie-message"]');
 new CookieMessage($cookieMessage).init();
 
-var $backLink = document.querySelector('[data-module="back-link"]');
+const $backLink = document.querySelector('[data-module="back-link"]');
 new BackLink($backLink).init();
 
-var $accordions = document.querySelectorAll('[data-module="accordion"]');
-for (var i = $accordions.length - 1; i >= 0; i--) {
-  var $accordion = $accordions[i];
-  var $sections = $accordion.querySelectorAll(".accordion-section");
-  for (var j = $sections.length - 1; j >= 0; j--) {
-    var $section = $sections[j];
-    var sectionContainsCheckedCheckboxes = !!$section.querySelector('.govuk-checkboxes input[type="checkbox"]:checked');
+const $accordions = document.querySelectorAll('[data-module="accordion"]');
+for (let i = $accordions.length - 1; i >= 0; i--) {
+  const $accordion = $accordions[i];
+  const $sections = $accordion.querySelectorAll(".accordion-section");
+  for (let j = $sections.length - 1; j >= 0; j--) {
+    let $section = $sections[j];
+    const sectionContainsCheckedCheckboxes = !!$section.querySelector(
+      '.govuk-checkboxes input[type="checkbox"]:checked'
+    );
     if (sectionContainsCheckedCheckboxes) {
       $section.classList.add("accordion-section--expanded");
     }
@@ -36,22 +38,22 @@ for (var i = $accordions.length - 1; i >= 0; i--) {
   try {
     new Accordion($accordion).init();
   } catch (e) {
-    for (var j = $sections.length - 1; j >= 0; j--) {
-      var $section = $sections[j];
+    for (let j = $sections.length - 1; j >= 0; j--) {
+      let $section = $sections[j];
       $section.classList.remove("accordion-section--hidden");
       $section.classList.add("accordion-section--expanded");
     }
   }
 }
 
-var $toggle = document.querySelectorAll('[data-module="toggle"]');
-for (var i = $toggle.length - 1; i >= 0; i--) {
+const $toggle = document.querySelectorAll('[data-module="toggle"]');
+for (let i = $toggle.length - 1; i >= 0; i--) {
   new Toggle($toggle[i]).init();
 }
 
 try {
-  var $locationAutocomplete = document.getElementById("location-autocomplete");
-  var $locationInput = document.getElementById("location");
+  const $locationAutocomplete = document.getElementById("location-autocomplete");
+  const $locationInput = document.getElementById("location");
   if ($locationAutocomplete) {
     initAutocomplete($locationAutocomplete, $locationInput);
   }
@@ -60,8 +62,8 @@ try {
 }
 
 try {
-  var $providerAutocomplete = document.getElementById("provider-autocomplete");
-  var $providerInput = document.getElementById("query");
+  const $providerAutocomplete = document.getElementById("provider-autocomplete");
+  const $providerInput = document.getElementById("query");
   if ($providerAutocomplete) {
     initAutocomplete($providerAutocomplete, $providerInput);
   }
@@ -74,10 +76,8 @@ if (typeof ga !== "undefined") {
   initExternalLinkAnalytics();
   initNavigationAnalytics();
 
-  var $page = document.querySelector('[data-module*="track-scrolling"]');
+  const $page = document.querySelector('[data-module*="ga-track"]');
   new ScrollTracking($page).init();
-
-  var $page = document.querySelector('[data-module*="track-copying"]');
   new CopyTracking($page).init();
 } else {
   /* istanbul ignore next */
--- src/Assets/app.test.js
+++ src/Assets/app.test.js
@@ -1,17 +1,17 @@
-import { initAll } from "govuk-frontend"
-import CookieMessage from "./Javascript/cookie-message"
-import BackLink from "./Javascript/back-link"
-import Accordion from "./Javascript/accordion"
-import Toggle from "./Javascript/toggle"
-import { initFormAnalytics, initExternalLinkAnalytics, initNavigationAnalytics } from "./Javascript/analytics.js"
-import initAutocomplete from "./Javascript/autocomplete"
+import { initAll } from "govuk-frontend";
+import CookieMessage from "./Javascript/cookie-message";
+import BackLink from "./Javascript/back-link";
+import Accordion from "./Javascript/accordion";
+import Toggle from "./Javascript/toggle";
+import { initFormAnalytics, initExternalLinkAnalytics, initNavigationAnalytics } from "./Javascript/analytics.js";
+import initAutocomplete from "./Javascript/autocomplete";
 
-jest.mock("govuk-frontend")
-jest.mock("./Javascript/cookie-message")
-jest.mock("./Javascript/back-link")
-jest.mock("./Javascript/accordion")
-jest.mock("./Javascript/toggle")
-jest.mock("./Javascript/autocomplete")
+jest.mock("govuk-frontend");
+jest.mock("./Javascript/cookie-message");
+jest.mock("./Javascript/back-link");
+jest.mock("./Javascript/accordion");
+jest.mock("./Javascript/toggle");
+jest.mock("./Javascript/autocomplete");
 
 describe("App", () => {
   beforeAll(() => {
@@ -32,45 +32,45 @@ describe("App", () => {
   <input type="text" id="provider">
   <div id="provider-autocomplete"></div>
 </div>
-`
+`;
 
-    require("./app")
-  })
+    require("./app");
+  });
 
   describe("initialising the app", () => {
     it("should initialise govuk-frontend", () => {
-      expect(initAll).toHaveBeenCalled()
-    })
+      expect(initAll).toHaveBeenCalled();
+    });
 
     it("should initialise CookieMessage", () => {
-      expect(CookieMessage).toHaveBeenCalled()
-    })
+      expect(CookieMessage).toHaveBeenCalled();
+    });
 
     it("should initialise BackLink", () => {
-      expect(BackLink).toHaveBeenCalled()
-    })
+      expect(BackLink).toHaveBeenCalled();
+    });
 
     it("should initialise BackLink", () => {
-      expect(BackLink).toHaveBeenCalled()
-    })
+      expect(BackLink).toHaveBeenCalled();
+    });
 
     describe("Accordion initialiser", () => {
       it("should call module", () => {
-        expect(Accordion).toHaveBeenCalled()
-      })
+        expect(Accordion).toHaveBeenCalled();
+      });
 
       it("should expand sections with ticked checkboxes", () => {
-        const $section = document.querySelector(".accordion-section")
-        expect($section.classList.contains("accordion-section--expanded")).toBe(true)
-      })
-    })
+        const $section = document.querySelector(".accordion-section");
+        expect($section.classList.contains("accordion-section--expanded")).toBe(true);
+      });
+    });
 
     it("should initialise Toggle", () => {
-      expect(Toggle).toHaveBeenCalled()
-    })
+      expect(Toggle).toHaveBeenCalled();
+    });
 
     it("should initialise Autocomplete", () => {
-      expect(initAutocomplete).toHaveBeenCalled()
-    })
-  })
-})
+      expect(initAutocomplete).toHaveBeenCalled();
+    });
+  });
+});
