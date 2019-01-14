f53330b267e0e37569098e6464c9605615df08fc
Dan Mitchell <dan@danmitchell.co.uk>
2019-01-14 11:39:50 +0000

    Bump govuk-frontend (#316)
    Includes official accordion component from govuk design system

--- src/Assets/app.test.js
+++ src/Assets/app.test.js
@@ -1,7 +1,6 @@
 import { initAll } from "govuk-frontend";
 import CookieMessage from "./Javascript/cookie-message";
 import BackLink from "./Javascript/back-link";
-import Accordion from "./Javascript/accordion";
 import Toggle from "./Javascript/toggle";
 import { initFormAnalytics, initExternalLinkAnalytics, initNavigationAnalytics } from "./Javascript/analytics.js";
 import initAutocomplete from "./Javascript/autocomplete";
@@ -9,7 +8,6 @@ import initAutocomplete from "./Javascript/autocomplete";
 jest.mock("govuk-frontend");
 jest.mock("./Javascript/cookie-message");
 jest.mock("./Javascript/back-link");
-jest.mock("./Javascript/accordion");
 jest.mock("./Javascript/toggle");
 jest.mock("./Javascript/autocomplete");
 
@@ -19,13 +17,6 @@ describe("App", () => {
 <div>
   <div data-module="cookie-message"></div>
   <div data-module="back-link"></div>
-  <div data-module="accordion">
-    <div class="accordion-section">
-      <div class="govuk-checkboxes">
-        <input type="checkbox" checked>
-      </div>
-    </div>
-  </div>
   <div data-module="toggle"></div>
   <input type="text" id="location">
   <div id="location-autocomplete"></div>
@@ -52,17 +43,6 @@ describe("App", () => {
 
     it("should initialise BackLink", () => {
       expect(BackLink).toHaveBeenCalled();
-    });
-
-    describe("Accordion initialiser", () => {
-      it("should call module", () => {
-        expect(Accordion).toHaveBeenCalled();
-      });
-
-      it("should expand sections with ticked checkboxes", () => {
-        const $section = document.querySelector(".accordion-section");
-        expect($section.classList.contains("accordion-section--expanded")).toBe(true);
-      });
     });
 
     it("should initialise Toggle", () => {
