e5233309dd477c545cb290a6d29a63f037f54a7a
Jake Benilov <benilov@gmail.com>
2019-03-22 15:28:29 +0000

    Simplify travis command for pushing nuget
    Have the environment variable called directly, like other scripts in the repo do.

--- travis-nuget-deploy.sh
+++ travis-nuget-deploy.sh
@@ -1,4 +1,5 @@
-ApiKey=$1
+#!/usr/bin/env bash
+set -e
 
 dotnet pack ./shared
-dotnet nuget push ./shared/bin/**/*.nupkg -k $ApiKey -s https://www.nuget.org || echo "Nuget deploy skipped"
+dotnet nuget push ./shared/bin/**/*.nupkg -k $NUGET_API_KEY -s https://www.nuget.org || echo "Nuget deploy skipped"
