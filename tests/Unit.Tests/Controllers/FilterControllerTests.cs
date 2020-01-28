ab715058cf2948136ec7a8273d8a3e1d900d0285
Tim Abell <tim@timwise.co.uk>
2020-01-28 16:37:46 +0000

    [2777] Redirect Qualifications filter to rails

--- tests/Unit.Tests/Controllers/FilterControllerTests.cs
+++ tests/Unit.Tests/Controllers/FilterControllerTests.cs
@@ -121,5 +121,30 @@ namespace SearchAndCompareUI.Tests.Unit.Tests.Controllers
             _redirectUrlMock.Verify(x => x.RedirectToNewApp(), Times.AtLeastOnce);
             result.Url.Should().Be(actualRedirectPath);
         }
+
+        [Test]
+        public void QualificationGetHttpGetTest()
+        {
+            var result = _filterController.QualificationGet(_resultsFilter);
+            result.Should().BeOfType<ViewResult>();
+            var viewResult = result as ViewResult;
+            viewResult?.Model.Should().Be(_resultsFilter);
+        }
+
+        [Test]
+        public void QualificationGetRedirectsToNewApp()
+        {
+            _mockFlag.Setup(x => x.RedirectToRails).Returns(true);
+            string actualRedirectPath = "results/filter/qualification?query";
+
+            var redirectObject = new RedirectResult(actualRedirectPath);
+            _redirectUrlMock.Setup(x => x.RedirectToNewApp())
+                .Returns(redirectObject);
+
+            var result = _filterController.QualificationGet(_resultsFilter) as RedirectResult;
+            result.Should().Be(redirectObject);
+            _redirectUrlMock.Verify(x => x.RedirectToNewApp(), Times.AtLeastOnce);
+            result.Url.Should().Be(actualRedirectPath);
+        }
     }
 }
